2b53bc186f91a9d1f22ff078ed85a411062ab400
alireza rahmani <150823639+alireza-rahmani991@users.noreply.github.com>
2026-08-25 19:19:45 +0330

    fixed database problems

--- src/main/java/com/example/digital_certificate/controller/CertificateController.java
+++ src/main/java/com/example/digital_certificate/controller/CertificateController.java
@@ -2,11 +2,24 @@ package com.example.digital_certificate.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.digital_certificate.entity.DigitalCertificate;
 import com.example.digital_certificate.service.CertificateService;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/certificates")
 public class CertificateController {
 
     private final CertificateService certificateService;
@@ -15,5 +28,40 @@ public class CertificateController {
         this.certificateService = certificateService;
     }
 
+    @PostMapping
+    public DigitalCertificate postNewCertificate(MultipartFile certificate) {
+        
+        return new DigitalCertificate();
+    }
+
+    @GetMapping
+    public String getCertificates() {
+        return new String();
+    }
+    
+    @GetMapping("/{id}")
+    public DigitalCertificate getCertificate(@PathVariable UUID id) {
+        return new DigitalCertificate();
+    }
+    
+    @DeleteMapping("/{id}")
+    public void deleteCertificate(@PathVariable UUID id) {
+        return;
+    }
+    
+    @PatchMapping("/{id}/revoke")
+    public DigitalCertificate revokeCertificate(@PathVariable UUID id) {
+        return new DigitalCertificate();
+    }
+
+    @GetMapping("/expiring")
+    public String getExpiringCertificates(@RequestParam UUID id) {
+        return new String();
+    }
+    
+    @GetMapping("/statistics")
+    public String getMethodName() {
+        return new String();
+    }
     
 }

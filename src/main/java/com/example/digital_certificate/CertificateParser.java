f8ae58a9dfbbced4fded2d924dd7eeeccb57195c
alireza rahmani <150823639+alireza-rahmani991@users.noreply.github.com>
2026-08-26 18:08:57 +0330

    added constructor for digital certificates and created certificate parser

--- src/main/java/com/example/digital_certificate/CertificateParser.java
+++ src/main/java/com/example/digital_certificate/CertificateParser.java
@@ -1,6 +1,58 @@
 package com.example.digital_certificate;
 
+import java.security.cert.X509Certificate;
+import java.time.Instant;
+
+import javax.naming.ldap.LdapName;
+import javax.naming.ldap.Rdn;
+
+import com.example.digital_certificate.entity.DigitalCertificate;
 
 public class CertificateParser {
-    
+
+    public DigitalCertificate parseCertificate(X509Certificate certificate) {
+
+        String serialNumber = certificate.getSerialNumber().toString();
+        String subject = certificate.getSubjectX500Principal().getName();
+        String issuer = certificate.getIssuerX500Principal().getName();
+        String commonName = getAttribute(subject, "CN");
+        String organization = getAttribute(subject, "O");
+        String country = getAttribute(subject, "C");
+        Instant validFrom = certificate.getNotBefore().toInstant();
+        Instant validTo = certificate.getNotAfter().toInstant();
+        String signatureAlgorithm = certificate.getSigAlgName();
+        String publicKeyAlgorithm = certificate.getPublicKey().getAlgorithm();
+
+        DigitalCertificate digitalCertificate = new DigitalCertificate(
+            serialNumber,
+            subject,
+            issuer,
+            commonName,
+            organization,
+            country,
+            validFrom,
+            validTo,
+            signatureAlgorithm,
+            publicKeyAlgorithm,
+            null,
+            null
+        );
+        return digitalCertificate;
+    }
+
+    String getAttribute(String subject, String type) {
+        try {
+            LdapName ldapName = new LdapName(subject);
+            for (Rdn rdn : ldapName.getRdns()) {
+                if (rdn.getType().equalsIgnoreCase(type)) {
+                    return rdn.getValue().toString();
+                }
+            }
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
--- src/main/java/com/example/digital_certificate/entity/DigitalCertificate.java
+++ src/main/java/com/example/digital_certificate/entity/DigitalCertificate.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 
 import com.example.digital_certificate.CertificateStatus;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,35 +23,76 @@ public class DigitalCertificate {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(name = "serial_number", nullable = false)
     private String serialNumber;
+    @Column(name = "subject", nullable = false)
     private String subject;
+    @Column(name = "issuer", nullable = false)
     private String issuer;
+    @Column(name = "common_name")
     private String commonName;
+    @Column(name = "organization")
     private String organization;
+    @Column(name = "country")
     private String country;
 
+    @Column(name = "valid_from", nullable = false)
     private Instant validFrom;
+    @Column(name = "valid_to", nullable = false)
     private Instant validTo;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
     private CertificateStatus status;
 
+    @Column(name = "signature_algorithm", nullable = false)
     private String signatureAlgorithm;
+    @Column(name = "public_key_algorithm", nullable = false)
     private String publicKeyAlgorithm;
+    @Column(name = "public_key_size", nullable = false)
     private Integer publicKeySize;
 
+    @Column(name = "fingerprint", nullable = false, unique = true)
     private String fingerprint;
+    @Column(name = "created_at", nullable = false)
     private Instant createdAt;
 
     public DigitalCertificate() {
     }
 
-    public UUID getId() {
-        return id;
+    public DigitalCertificate(String serialNumber, String subject, String issuer, String commonName,
+            String organization, String country, Instant validFrom, Instant validTo,
+            String signatureAlgorithm, String publicKeyAlgorithm, Integer publicKeySize, String fingerprint) {
+        this.serialNumber = serialNumber;
+        this.subject = subject;
+        this.issuer = issuer;
+        this.commonName = commonName;
+        this.organization = organization;
+        this.country = country;
+        this.validFrom = validFrom;
+        this.validTo = validTo;
+        this.signatureAlgorithm = signatureAlgorithm;
+        this.publicKeyAlgorithm = publicKeyAlgorithm;
+        this.publicKeySize = publicKeySize;
+        this.fingerprint = fingerprint;
+        this.createdAt = Instant.now();
+
+        Instant now = Instant.now();
+        if(validFrom.isAfter(now)){
+            this.status = CertificateStatus.NOT_YET_VALID;
+        }
+        else if(validTo.isBefore(now)){
+            this.status = CertificateStatus.EXPIRED;
+        }
+        else{
+            this.status = CertificateStatus.VALID;
+        }
+
+
     }
 
-    public void setId(UUID id) {
-        this.id = id;
+    public UUID getId() {
+        return id;
     }
 
     public String getSerialNumber() {
@@ -160,7 +203,18 @@ public class DigitalCertificate {
         return createdAt;
     }
 
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
+    @PrePersist
+    public void setCreatedAt() {
+        this.createdAt = Instant.now();
     }
+
+    @Override
+    public String toString() {
+        return "DigitalCertificate [id=" + id + "\n serialNumber=" + serialNumber + "\n subject=" + subject + "\n issuer="
+                + issuer + "\n commonName=" + commonName + "\n organization=" + organization + "\n country=" + country
+                + "\n validFrom=" + validFrom + "\n validTo=" + validTo + "\n status=" + status + "\n signatureAlgorithm="
+                + signatureAlgorithm + "\n publicKeyAlgorithm=" + publicKeyAlgorithm + "\n publicKeySize=" + publicKeySize
+                + "\n fingerprint=" + fingerprint + "\n createdAt=" + createdAt + "]";
+    }
+
 }
